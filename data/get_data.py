8d4f1d41a046e14b59c1ae9b6cf5259858f30540
James McDermott <jamesmichaelmcdermott@gmail.com>
2013-04-28 17:57:56 +0100

    Added a directory to hold data files, and a script to get them (doesn't do much yet).

---
+++ data/get_data.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+urls = [
+    "http://personal.disco.unimib.it/Vanneschi/bioavailability.txt",
+    "http://personal.disco.unimib.it/Vanneschi/bioavailability_lookup.txt"
+    ]
+
+print("For now, just download the above files manually.")
