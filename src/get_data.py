3d35c41dd3ebc40ea2f876edd044d08aa614cbd7
James McDermott <jamesmichaelmcdermott@gmail.com>
2013-05-02 18:34:35 +0100

    Added some new URLs to the get_data script.

--- src/get_data.py
+++ src/get_data.py
@@ -28,25 +28,40 @@ def pull_file(url):
     f.close()
 
 def main():
-    urls = ["http://personal.disco.unimib.it/Vanneschi/bioavailability.txt",
-            "http://personal.disco.unimib.it/Vanneschi/bioavailability_lookup.txt",
-            "http://personal.disco.unimib.it/Vanneschi/toxicity.txt",
-            "http://www.iwr.uni-heidelberg.de/groups/comopt/software/TSPLIB95/tsp/ALL_tsp.tar.gz",
-            "http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/STSP.html"
-            # TODO download all the Hamiltonian cycle probems,
-            # capacitated arc-routing problems, etc from the same
-            # source as TSPLIB
-            ]
+    urls = [
+        # "http://personal.disco.unimib.it/Vanneschi/bioavailability.txt",
+        # "http://personal.disco.unimib.it/Vanneschi/bioavailability_lookup.txt",
+        # "http://personal.disco.unimib.it/Vanneschi/toxicity.txt",
+        # "http://www.iwr.uni-heidelberg.de/groups/comopt/software/TSPLIB95/tsp/ALL_tsp.tar.gz",
+        # "http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/STSP.html",
+        # TODO download all the Hamiltonian cycle probems,
+        # capacitated arc-routing problems, etc from the same
+        # source as TSPLIB
+        # "http://casnew.iti.upv.es/index.php/evocompetitions/105-symregcompetition", # temporarily down?
+        "http://symbolicregression.com/sites/default/files/DataSets/towerData.txt",
+        "http://symbolicregression.com/sites/SRDocuments/DataSets/EmulatorProblem.zip",
+        ]
 
     for url in urls:
         pull_file(url)
-    os.makedirs("TSPLIB")
-    os.rename("ALL_tsp.tar.gz", "TSPLIB/ALL_tsp.tar.gz")
-    # TODO this should be platform-independent
-    os.system("cd TSPLIB; tar xzf ALL_tsp.tar.gz")
-    os.rename("STSP.html", "TSPLIB/STSP.html")
+    try:
+        os.makedirs("TSPLIB")
+    except OSError:
+        pass
+    # os.rename("ALL_tsp.tar.gz", "TSPLIB/ALL_tsp.tar.gz")
+    # # TODO this should be platform-independent
+    # os.system("cd TSPLIB; tar xzf ALL_tsp.tar.gz")
+    # os.rename("STSP.html", "TSPLIB/STSP.html")
+    os.system("unzip EmulatorProblem.zip")
+    os.remove("__MACOSX/EmulatorProblem/._EmpiricalEmulators.pdf")
+    os.removedirs("__MACOSX")
+    os.remove("EmulatorProblem.zip")
+    os.remove("TSPLIB/ALL_tsp.tar.gz")
     
 if __name__ == '__main__':
-    os.makedirs("../data")
+    try:
+        os.makedirs("../data")
+    except OSError:
+        pass
     os.chdir("../data")
     main()
